8c7dd0fb50cfd9451f3cd2e470880a95ddb13516
tenfortyeight <john@magnusson.gl>
2015-05-22 16:45:38 +0200

    Connecting to backend

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
     app = express.createServer();
 
 var http = require('http'),
-    config = require('./config');
+    config = require('./config')();
 
 app.configure(function () {
 });
@@ -13,19 +13,46 @@ app.listen(config.appPort);
 var Twitter = require('twitter');
  
 var client = new Twitter({
-  consumer_key: config.tweetApiKey,
+  consumer_key: config.twitterApiKey,
   consumer_secret: config.twitterSecret,
   access_token_key: config.twitterAccessToken,
   access_token_secret: config.twitterAccessSecret
 });
+
+console.log(client);
  
-var params = {screen_name: 'nodejs'};
-client.stream('statuses/filter', {track: 'javascript'}, function(stream) {
+var params = {screen_name: '@letsGoChatHere'};
+
+client.stream('statuses/filter', {track: '@letsGoChatHere'}, function(stream) {
+    console.log("Stream start");
+
   stream.on('data', function(tweet) {
-    console.log(tweet.text);
+    var jsonTweet = JSON.stringify(tweet);
+    var post_options = {
+      host: 'localhost',
+      port: '4444',
+      path: '/api/discussions',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': jsonTweet.length
+      }
+    };
+
+    var post_req = http.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log('Response: ' + chunk);
+      });
+    });
+
+    // post the data
+    post_req.write(jsonTweet);
+    post_req.end();
   });
  
   stream.on('error', function(error) {
+    console.log(error);
     throw error;
   });
 });
